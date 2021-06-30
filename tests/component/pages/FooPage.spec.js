8660190657bb0bd665648783d8c607f0d811605e
Thorsten Kühner <thorsten.kuehner@daimler.com>
2021-06-30 12:48:04 +0200

    Add component test case for increment counter

--- tests/component/pages/FooPage.spec.js
+++ tests/component/pages/FooPage.spec.js
@@ -5,7 +5,13 @@ import FooPage from '../../../src/pages/FooPage.vue';
 describe('FooPage component', () => {
     it('should render heading', () => {
         mount(FooPage);
-
         cy.contains('Foo Page').should('be.visible');
+    });
+
+    it('should increment counter', () => {
+        mount(FooPage);
+        cy.get('.counter').should('contain', 0);
+        cy.get('button').click();
+        cy.get('.counter').should('contain', 1);
     });
 });
